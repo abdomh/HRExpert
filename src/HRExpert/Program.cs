35baaa3136ef76ef438b3b5080751aee808e94ec
artemb89 <artem_b89@mail.ru>
2016-06-08 12:01:40 +0300

    1

--- src/HRExpert/Program.cs
+++ src/HRExpert/Program.cs
@@ -14,13 +14,16 @@ namespace HRExpert
         /// <param name="args"></param>
         public static void Main(string[] args)
         {
+            System.Console.WriteLine("Applications start.");
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
+            System.Console.WriteLine("Pre run");
             host.Run();
+            System.Console.WriteLine("OK.");
         }
     }
 }
